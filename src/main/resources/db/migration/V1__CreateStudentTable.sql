ce9b2863b41f63fc06315af0e55bc707b6238037
vladandrei51 <vladandrei51@gmail.com>
2020-02-18 11:13:30 +0200

    adapted db.migrations + delete unnecessary files

--- src/main/resources/db/migration/V1__CreateStudentTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS user (
-    userId UUID PRIMARY KEY NOT NULL,
-    firstName VARCHAR(100) NOT NULL,
-    lastName VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password  VARCHAR(100) NOT NULL
-);
-
---
+++ src/main/resources/db/migration/V1__CreateTables.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS user (
+    userId UUID PRIMARY KEY NOT NULL,
+    firstName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password  VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS alarm(
+    alarmId UUID PRIMARY KEY NOT NULL,
+    userId UUID NOT NULL,
+    stockSymbol VARCHAR(10) NOT NULL,
+    targetAlarmPercentage INT,
+    currentAlarmVariance INT,
+    initialStockPrice FLOAT,
+    currentStockPrice FLOAT,
+    isActive BIT
+);
+
