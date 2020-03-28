076aabd5d162374e010ce430dabd7eb5f208ce03
vladandrei51 <vladandrei51@gmail.com>
2020-03-28 15:41:58 +0200

    Removed js folder

--- src/js/src/Container.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Container = props => (
-    <div style={{width: '1400px', margin: '0 auto', textAlign: 'center'}}>
-        {props.children}
-    </div>
-);
-
-export default Container;
